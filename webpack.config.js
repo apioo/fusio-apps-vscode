1110152f9eb2508aaf02034bd44ede18a7f3e832
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-07-03 21:12:47 +0200

    webpack disable concatenate modules

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     mode: 'production',
     entry: {
-        "app": './src/index.js',
+        'app': './src/index.js',
     },
     output: {
         globalObject: 'self',
@@ -13,16 +13,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         publicPath: 'dist/'
     },
+    optimization: {
+        concatenateModules: false
+    },
     module: {
         rules: [{
             test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
+            use: ['style-loader', 'css-loader']
         }]
     },
     plugins: [
         new CleanWebpackPlugin(),
         new MonacoWebpackPlugin({
-            languages: ["javascript", "php", "json", "typescript", "sql", "yaml"]
+            languages: ['javascript', 'php', 'json', 'typescript', 'sql', 'yaml']
         })
     ]
 };
